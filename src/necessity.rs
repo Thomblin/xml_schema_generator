1204225e9c79b05aac0681b9cac10bd22f80e583
Sebastian Detert <github@elygor.de>
2023-10-03 13:30:48 +0200

    refactor: improvements as suggest by linter

--- src/necessity.rs
+++ src/necessity.rs
@@ -5,7 +5,7 @@
 #[derive(Debug)]
 pub enum Necessity<T> {
     Optional(T),
-    Mandatory(T)
+    Mandatory(T),
 }
 
 impl<T: std::cmp::PartialEq> std::cmp::PartialEq for Necessity<T> {
@@ -19,12 +19,12 @@ impl<T: std::cmp::PartialEq> std::cmp::PartialEq for Necessity<T> {
     }
 }
 
-impl<T> Necessity<T> {  
+impl<T> Necessity<T> {
     /// return the inner element
     pub fn into_inner_t(self) -> T {
         match self {
             Necessity::Optional(t) => t,
-            Necessity::Mandatory(t) => t
+            Necessity::Mandatory(t) => t,
         }
     }
 
@@ -32,7 +32,7 @@ impl<T> Necessity<T> {
     pub fn inner_t(&self) -> &T {
         match self {
             Necessity::Optional(t) => t,
-            Necessity::Mandatory(t) => t
+            Necessity::Mandatory(t) => t,
         }
     }
 
@@ -40,7 +40,7 @@ impl<T> Necessity<T> {
     pub fn inner_t_mut(&mut self) -> &mut T {
         match self {
             Necessity::Optional(t) => t,
-            Necessity::Mandatory(t) => t
+            Necessity::Mandatory(t) => t,
         }
     }
 }
@@ -49,31 +49,34 @@ impl<T> Necessity<T> {
 /// the result contains mandatory elements if they were present as mandatory in both given lists
 /// all other elements are returned as optional
 /// elements of the second list are appended to the first list
-/// 
+///
 /// Example
 /// ```
 /// use xml_schema_generator::Necessity;
-/// 
+///
 /// let vec: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Mandatory(2), Necessity::Mandatory(4) ];
 /// let other: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Mandatory(3), Necessity::Optional(4)];
 /// let expected: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Optional(2), Necessity::Optional(4), Necessity::Optional(3)];
-/// 
+///
 /// let actual = xml_schema_generator::merge_necessity(vec, other);
-/// 
+///
 /// assert_eq!(actual, expected);
 /// ```
-pub fn merge_necessity<T: std::cmp::PartialEq>(vec: Vec<Necessity<T>>, other: Vec<Necessity<T>>) -> Vec<Necessity<T>> {  
+pub fn merge_necessity<T: std::cmp::PartialEq>(
+    vec: Vec<Necessity<T>>,
+    other: Vec<Necessity<T>>,
+) -> Vec<Necessity<T>> {
     let mut result: Vec<Necessity<T>> = Vec::new();
-       
+
     for vec_item in vec.into_iter() {
         let mut found = false;
         let mut optional = true;
         for other_item in other.iter() {
             if other_item.inner_t() == vec_item.inner_t() {
                 found = true;
-                match (&other_item, &vec_item) {
-                    (Necessity::Mandatory(_), Necessity::Mandatory(_)) => optional = false,
-                    _ => (),
+                if let (Necessity::Mandatory(_), Necessity::Mandatory(_)) = (&other_item, &vec_item)
+                {
+                    optional = false
                 }
                 break;
             }
@@ -82,7 +85,7 @@ pub fn merge_necessity<T: std::cmp::PartialEq>(vec: Vec<Necessity<T>>, other: Ve
             true => match optional {
                 true => result.push(Necessity::Optional(vec_item.into_inner_t())),
                 false => result.push(Necessity::Mandatory(vec_item.into_inner_t())),
-            },            
+            },
             false => result.push(Necessity::Optional(vec_item.into_inner_t())),
         }
     }
@@ -112,7 +115,11 @@ mod tests {
     fn merge_necessity_changes_necessity_states() {
         let vec: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Mandatory(2)];
         let other: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Mandatory(3)];
-        let expected: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Optional(2), Necessity::Optional(3)];
+        let expected: Vec<Necessity<u8>> = vec![
+            Necessity::Mandatory(1),
+            Necessity::Optional(2),
+            Necessity::Optional(3),
+        ];
 
         let actual = merge_necessity(vec, other);
 
@@ -121,9 +128,21 @@ mod tests {
 
     #[test]
     fn merge_necessity_keeps_optional() {
-        let vec: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Optional(2), Necessity::Optional(3)];
-        let other: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Mandatory(2), Necessity::Optional(3)];
-        let expected: Vec<Necessity<u8>> = vec![Necessity::Mandatory(1), Necessity::Optional(2), Necessity::Optional(3)];
+        let vec: Vec<Necessity<u8>> = vec![
+            Necessity::Mandatory(1),
+            Necessity::Optional(2),
+            Necessity::Optional(3),
+        ];
+        let other: Vec<Necessity<u8>> = vec![
+            Necessity::Mandatory(1),
+            Necessity::Mandatory(2),
+            Necessity::Optional(3),
+        ];
+        let expected: Vec<Necessity<u8>> = vec![
+            Necessity::Mandatory(1),
+            Necessity::Optional(2),
+            Necessity::Optional(3),
+        ];
 
         let actual = merge_necessity(vec, other);
 
