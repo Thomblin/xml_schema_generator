521a3645138e2c82be59a02e5b68c692e715a222
Sebastian Detert <github@elygor.de>
2023-11-26 00:09:54 +0100

    test: added test case with serde_xml_rs

---
+++ tests/issue7.rs
@@ -0,0 +1,55 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct MvciModuleDescription {
+    #[serde(rename = "@FILE_VERSION")]
+    pub file_version: String,
+    #[serde(rename = "@MVCI_PART2_STANDARD_VERSION")]
+    pub mvci_part2_standard_version: String,
+    #[serde(rename = "$text")]
+    pub text: Option<String>,
+    #[serde(rename = "PINTYPE")]
+    pub pintype: Vec<Pintype>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Pintype {
+    #[serde(rename = "ID")]
+    pub id: String,
+    #[serde(rename = "SHORT_NAME")]
+    pub short_name: String,
+    #[serde(rename = "@EID")]
+    pub eid: String,
+    #[serde(rename = "$text")]
+    pub text: Option<String>,
+}
+
+#[test]
+fn handle_attribute_prefixed_with_at() {
+    let xml = "\
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<MVCI_MODULE_DESCRIPTION FILE_VERSION=\"1.22.2.0\" MVCI_PART2_STANDARD_VERSION=\"2.1.1\">
+    <PINTYPE EID=\"ID_HI\">
+        <ID>0</ID>
+        <SHORT_NAME>HI</SHORT_NAME>
+    </PINTYPE>
+    <PINTYPE EID=\"ID_LOW\">
+        <ID>1</ID>
+        <SHORT_NAME>LOW</SHORT_NAME>
+    </PINTYPE>
+</MVCI_MODULE_DESCRIPTION>
+";
+
+    let mut reader = quick_xml::reader::Reader::from_str(xml);
+    if let Ok(root) = xml_schema_generator::into_struct(&mut reader) {
+        println!("{}", root.to_serde_struct());
+    }
+
+    let root: MvciModuleDescription = quick_xml::de::from_str(xml).unwrap();
+
+    assert_eq!("1.22.2.0".to_string(), root.file_version);
+
+    let root: MvciModuleDescription = serde_xml_rs::from_str(xml).unwrap();
+
+    assert_eq!("1.22.2.0".to_string(), root.file_version);
+}
