9c516972c1b42f2270e36d90e345dfa4a0da6fbb
Sebastian Detert <github@elygor.de>
2024-07-20 22:00:56 +0200

    Refactoring (#39)
    * fail if code coverage is below 80%
    
    * run tests for wasm folder for each push
    
    * moved deprecated config file
    
    * refactor: added docs
    
    * test: added integration test
    
    * fail if wasm code coverage is below 75%

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,19 +1,50 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
 use quick_xml::reader::Reader;
+use wasm_bindgen::prelude::*;
 use xml_schema_generator::{into_struct, Options};
 
 #[wasm_bindgen]
-pub fn transform_xml(s: &str) -> String{
+pub fn transform_xml(s: &str) -> String {
     let mut reader = Reader::from_str(s);
-    
+
     match into_struct(&mut reader) {
-        Ok(root) => {
-            root.to_serde_struct(&Options::quick_xml_de())
-        }
+        Ok(root) => root.to_serde_struct(&Options::quick_xml_de()),
         Err(e) => {
             format!("{:?}", e)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::transform_xml;
+
+    #[test]
+    fn transform_xml_returns_struct() {
+        let xml = "<foo>hello</foo>";
+
+        let actual = transform_xml(xml);
+
+        let expected = concat!(
+            "#[derive(Serialize, Deserialize)]\n",
+            "pub struct Foo {\n",
+            "    #[serde(rename = \"$text\")]\n",
+            "    pub text: Option<String>,\n",
+            "}\n\n"
+        );
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn transform_xml_returns_error() {
+        let xml = "{foo: 'hello'}";
+
+        let actual = transform_xml(xml);
+
+        let expected = "ParsingError(\"invalid XML, no root element found\")";
+
+        assert_eq!(expected, actual);
     }
 }
