772fecb979152bf1f1a149de3c99d0273fdef3b9
Sebastian Detert <github@elygor.de>
2024-07-20 21:34:01 +0200

    style: fixes

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,17 +1,15 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
 use quick_xml::reader::Reader;
+use wasm_bindgen::prelude::*;
 use xml_schema_generator::{into_struct, Options};
 
 #[wasm_bindgen]
-pub fn transform_xml(s: &str) -> String{
+pub fn transform_xml(s: &str) -> String {
     let mut reader = Reader::from_str(s);
-    
+
     match into_struct(&mut reader) {
-        Ok(root) => {
-            root.to_serde_struct(&Options::quick_xml_de())
-        }
+        Ok(root) => root.to_serde_struct(&Options::quick_xml_de()),
         Err(e) => {
             format!("{:?}", e)
         }
